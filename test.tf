36150e7e28ee27beb043dcd8965a83dc9ced63c9
Chakri <chakravarthishroff24@gmail.com>
2023-04-25 08:25:10 +0530

    test terraform

--- test.tf
+++ test.tf
@@ -1,8 +1,7 @@
-resource "aws_instance" "web" {
-  ami           = ami-0b5a2b5b8f2be4ec2
+resource "aws_instance" "dev" {
+  ami = "ami-0b5a2b5b8f2be4ec2"
   instance_type = "t3.micro"
-
   tags = {
-    Name = "HelloWorld"
+    Name = "Hello World"
   }
 }
