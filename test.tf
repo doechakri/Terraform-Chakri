0c6cca2b1b6932006b36455737eda1f4b3a81159
Chakri <chakravarthishroff24@gmail.com>
2023-04-25 08:25:10 +0530

    test terraform

--- test.tf
+++ test.tf
@@ -1,8 +1,11 @@
-resource "aws_instance" "web" {
-  ami           = ami-0b5a2b5b8f2be4ec2
+resource "aws_instance" "dev" {
+  ami = "ami-0b5a2b5b8f2be4ec2"
   instance_type = "t3.micro"
-
   tags = {
-    Name = "HelloWorld"
+    Name = "Hello World"
   }
+}
+data "aws_ami" "centos8" {
+  most_recent = true
+  owners = [973714476881]
 }
